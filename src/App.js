03b50d01c6e2c214bf95bc497a21643702430244
GastonLell <gastonglell@gmail.com>
2022-06-15 16:56:43 -0300

    agregar editar y eliminar tareas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 import './App.css';
 import ContactList from './components/container/ContactList';
 import TaskList from './components/container/TaskList';
-
 function App() {
   return (
     <div className="App">
       <TaskList/>
-      <ContactList/>
+      {/* <ContactList/> */}
     </div>
   );
 }
--- src/components/pure/Task.jsx
+++ src/components/pure/Task.jsx
@@ -1,29 +1,71 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Task } from '../../models/task.class';
+import { LEVELS } from '../../models/levels.enum';
 
-const TaskComponent = ({ task }) => {
+const TaskComponent = ({ task, complete, remove}) => {
+
+    function taskLevelBadge() {
+        switch (task.level) {
+            case LEVELS.NORMAL:
+                return (
+                    <h6 className='mb-0'>
+                        <span className='badge bg-primary'>
+                            {task.level}
+                        </span>
+                    </h6>
+                )
+            case LEVELS.URGENTE:
+                return (
+                    <h6 className='mb-0'>
+                        <span className='badge bg-warning'>
+                            {task.level}
+                        </span>
+                    </h6>
+                )
+            case LEVELS.BLOCKING:
+                return (
+                    <h6 className='mb-0'>
+                        <span className='badge bg-danger'>
+                            {task.level}
+                        </span>
+                    </h6>
+                )
+            default:
+                break;
+        }
+    }
+
+    function taskIconCompleted() {
+        return task.completed
+            ? (<i onClick={() => complete(task)} className='bi-toggle-on' style={{ color: 'green' }}></i>)
+            : (<i onClick={() => complete(task)} className='bi-toggle-off' style={{ color: 'grey' }}></i>)
+    }
     return (
-        <div>
-            <h2>
-                Nombre: {task.name}
-            </h2>
-            <h3>
-                Descripcion: {task.description}
-            </h3>
-            <h4>
-                Level: {task.level}
-            </h4>
-
-            <h5>
-                This task is: { task.completed ? 'COMPLETED' : 'PENDING'}
-            </h5>
-        </div>
+        <tr className='fw-normal'>
+            <th>
+                <span className='ms-2'>{task.name}</span>
+            </th>
+            <td className='align-middle'>
+                <span className='ms-2'>{task.description}</span>
+            </td>
+            <td className='align-middle'>
+                {taskLevelBadge()}
+            </td>
+            <td className='align-middle'>
+                {
+                    taskIconCompleted()
+                }
+                <i className='bi-trash' onClick={() => remove(task)} style={{ color: 'tomato', fontWeight: 'bold' }}></i>
+            </td>
+        </tr>
     )
 }
 
 TaskComponent.propTypes = {
-    task: PropTypes.instanceOf(Task)
+    task: PropTypes.instanceOf(Task).isRequired,
+    complete: PropTypes.func.isRequired,
+    remove: PropTypes.func.isRequired,
 }
 
 export default TaskComponent;
