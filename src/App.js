7553acd81c2bee017f08e67d3214cb4875298dfd
GastonLell <gastonglell@gmail.com>
2022-06-24 08:45:24 -0300

    impleents formik in login

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import './App.css';
 import ContactList from './components/container/ContactList';
 import TaskList from './components/container/TaskList';
+import LoginFormik from './components/pure/forms/LoginFormik';
 function App() {
   return (
     <div className="App">
-      <TaskList/>
+      <LoginFormik/>
       {/* <ContactList/> */}
     </div>
   );
